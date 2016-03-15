0a401aed64b42a62c94de9658441970966e724b8
Fr0hman <stenmatik@gmail.com>
2016-03-15 18:52:51 +0200

    Uus test. (Kas kausta puhul on äkki tegemist failiga?)

--- jaga.sh
+++ jaga.sh
@@ -3,7 +3,7 @@
 #
 #Script jagab etteantud grupile kasutamiseks uue kausta.
 #
-# V0.2
+# V0.2.1
 
 export LC_ALL=C
 
@@ -11,7 +11,7 @@ export LC_ALL=C
 if [ $UID -ne 0 ]
 then
   echo "käivita skript juurkasutaja õigustes"
-  exit 0
+  exit 1
 fi
 
 #Kontrollib, kas on ette antud õige arv muutujaid
@@ -28,7 +28,7 @@ else
     SHARE=$3
   else
     echo "kasuta sktipti nii: $(basename $0) KAUST GRUPP [SHARE]"
-    exit 0
+    exit 1
   fi
 fi
 
@@ -36,7 +36,7 @@ fi
 if [ $KAUST == "." ] || [ $KAUST == "/" ]
 then
   echo "Kausta parameeter valesti sisestatud."
-  exit 0
+  exit 1
 fi
 
 #Kontrollib, kas samba on paigaldatud (vajadusel paigaldab)
@@ -45,10 +45,11 @@ type smbd > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
   echo "Paigaldan samba."
-  apt-get update > /dev/null 2>&1 && apt-get install samba -y > /dev/null 2>&1 || exit 1 
+  apt-get update > /dev/null 2>&1 && apt-get install samba -y > /dev/null 2>&1 || exit 1
 fi
 
-#Kontrollib, kas kaust on olemas (vajadusel loob)
+#Kontrollib, kas kaust (või fail?) on olemas (vajadusel loob)
+test -e $KAUST && echo "See on fail, mitte kaust!" && exit 1
 test -d $KAUST || echo "Kausta pole. Loon kausta!" && mkdir -p $KAUST
 sleep 0.1
 
@@ -57,7 +58,7 @@ getent group $GRUPP > /dev/null 2>&1 || echo "Gruppi pole. Loon grupi!" && addgr
 sleep 0.1
 
 #Kas selline share on juba olemas?
-grep $SHARE /etc/samba/smb.conf > /dev/null && echo "Selline share ($SHARE) juba on olemas!" && exit 0
+grep $SHARE /etc/samba/smb.conf > /dev/null && echo "Selline share ($SHARE) juba on olemas!" && exit 1
 
 #Teeb konfifailist varukoopia
 echo "Teen /etc/samba/smb.conf failist varukoopia"
@@ -86,7 +87,7 @@ if [ $? -eq 0 ]
     echo "Conf OK!"
   else
     echo "Conf ERROR!"
-    exit 0
+    exit 1
 fi
 sleep 0.1
 
@@ -98,4 +99,7 @@ sleep 0.1
 #Reload sambale
 echo "Teen sambale reloadi."
 sleep 0.1
-/etc/init.d/smbd reload > /dev/null 2>&1 || echo "Reloadi ei toimunud, midagi läks valesti." && exit 0
+
+/etc/init.d/smbd reload > /dev/null 2>&1 || echo "Reloadi ei toimunud, midagi läks valesti." && exit 1
+
+exit 0
