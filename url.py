ad259f985b7f4e75e4408181c7b137a2e2b375ea
Fr0hman <stenmatik@gmail.com>
2016-05-17 17:35:18 +0300

    Update url.py
    Ver 1.1
    
    *Shows progress if wanted.
    *Shows how many url-s were looked at.

--- url.py
+++ url.py
@@ -7,16 +7,21 @@
 # ("Yes" or "No") into the output file. (parameter 2)
 # Also allows to use a third parameter for selecting to write(w) or append(a) to output file.
 #
+# Version 1.1
+#
 #################################################################################################
 
 import sys
 import os
 import urllib.request
 
+# showProgress variable, change this if you want
+showProgress = True
+
 # Check if parameters are given
 if len(sys.argv) < 3:
     print("Usage:", sys.argv[0], "<FileContainingURLs> <FileToWriteTo> [a / w]")
-    print("<Mandatory> [optional]")
+    print("\n<Mandatory> [optional]")
     exit(2)
 
 # Set and check if extra argument is given (append or write)
@@ -25,7 +30,7 @@ if len(sys.argv) == 4:
     if (sys.argv[3] == 'a') or (sys.argv[3] == 'w'):
         option = sys.argv[3]
 
-# Check if input file exists, if it does, open both files, input in read and output in append mode
+# Check if input file exists, if it does, open both files, input in read and output in selected mode
 if os.path.isfile(sys.argv[1]):
     fIn = open(sys.argv[1], 'r')
     fOut = open(sys.argv[2], option)
@@ -33,23 +38,31 @@ else:
     print("The file you are trying to read does not exist.")
     exit(2)
 
-# Set numError variable for number of lines skipped
+# Set numError variable for number of lines skipped and lines variable for total lines
 numError = 0
+lines = 0
 
-# For every line we check if the given URL has matching string
+# For every line, we check if the given URL has matching string
 for line in fIn:
 
-# Define variables for simpler code
+    if showProgress == True:
+        print("*")
+    lines += 1
+
     pair = (str.split(line))
     url = pair[0]
-    if not url.startswith('http://'):   # Prepend "http://" in case it is missing
+
+    # Prepend "http://" in case it is missing
+    if not url.startswith('http://'):
         url = 'http://' + url
-    if len(pair) < 2:   # Check if search word is missing or not
+
+    # Check if search word is missing or not
+    if len(pair) < 2:
         numError += 1
         continue
     searchString = pair[1]
 
-# Try to get the source code, if it fails, skip iteration
+    # Try to get the source code, if it fails, skip iteration
     try:
         response = urllib.request.urlopen(url)
     except urllib.request.URLError:
@@ -57,20 +70,26 @@ for line in fIn:
         continue
     source = response.read()
 
-# If the source code has searchString in it, the value is "Yes", else "No"
+    # If the source code has searchString in it, the value is "Yes", else "No"
     if str(source).find(searchString) == -1:
-        value = "Yes"
-    else:
         value = "No"
+    else:
+        value = "Yes"
 
-# If everything went well, write the line to output file
+    # If everything went well, write the line to output file
     fOut.write(url + " " + searchString + " " + value + "\n")
 
 # Print the outcome and number of errors if there were any
 if numError > 0:
-    print("Finished with ", numError, " errors.")
+    print("Checked", lines, "URL-s from", sys.argv[1])
+    print("Wrote output to", sys.argv[2])
+    print("#######################################")
+    print("Finished with", numError, "errors.")
     exit(1)
 else:
+    print("Checked", lines, "URL-s from", sys.argv[1])
+    print("Wrote output to", sys.argv[2])
+    print("#######################################")
     print("Finished without errors.")
 
 exit(0)
